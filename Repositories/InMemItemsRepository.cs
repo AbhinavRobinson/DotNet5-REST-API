5d389be42d8a96e102b1ac96771f01d9639938d1
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-12 18:45:36 +0530

    (feat): add item and repository

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using DotNet5_REST_API.Entities;
+using System.Linq;
+
+namespace DotNet5_REST_API.Repositories
+{
+  public class InMemItemsRepository
+  {
+    private readonly List<Item> items = new()
+    {
+      new Item
+      {
+        Id = Guid.NewGuid(),
+        Name = "Potion",
+        Price = 9,
+        CreatedDate = DateTimeOffset.UtcNow
+      },
+      new Item
+      {
+        Id = Guid.NewGuid(),
+        Name = "Iron Sword",
+        Price = 20,
+        CreatedDate = DateTimeOffset.UtcNow
+      },
+      new Item
+      {
+        Id = Guid.NewGuid(),
+        Name = "Bronze Shield",
+        Price = 18,
+        CreatedDate = DateTimeOffset.UtcNow
+      }
+    };
+
+    public IEnumerable<Item> GetItems()
+    {
+      return items;
+    }
+
+    public Item GetItem(Guid id)
+    {
+      return items.Where(item => item.Id == id).SingleOrDefault();
+    }
+  }
+}
